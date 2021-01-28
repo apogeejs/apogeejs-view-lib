f170cd19915259903bbad30f4f330b99ab3a7c6a
sutter-dave <sutter@intransix.com>
2021-01-28 15:44:42 -0800

    split out namespaces to separate better the app and web client lib

--- childComponentViewConfig.js
+++ childComponentViewConfig.js
@@ -1,7 +1,5 @@
 import JsonTableComponentView from "/apogeeview/componentviews/JsonTableComponentView.js";
-import FolderComponentView from "/apogeeview/componentviews/FolderComponentView.js";
 import FunctionComponentView from "/apogeeview/componentviews/FunctionComponentView.js";
-import FolderFunctionComponentView from "/apogeeview/componentviews/FolderFunctionComponentView.js";
 import DynamicFormView from "/apogeeview/componentviews/DynamicFormView.js";
 import FormDataComponentView from "/apogeeview/componentviews/FormDataComponentView.js";
 import CustomComponentView from "/apogeeview/componentviews/CustomComponentView.js";
@@ -10,10 +8,10 @@ import ErrorComponentView from "/apogeeview/componentviews/ErrorComponentView.js
 
 import {registerComponentView,setErrorComponentView} from "/apogeeview/componentViewInfo.js";
 
+//registration of the child component views
+
 registerComponentView(JsonTableComponentView);
-registerComponentView(FolderComponentView);
 registerComponentView(FunctionComponentView);
-registerComponentView(FolderFunctionComponentView);
 registerComponentView(ErrorComponentView);
 registerComponentView(DynamicFormView);
 registerComponentView(FormDataComponentView);
