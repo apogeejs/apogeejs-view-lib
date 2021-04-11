b74de0faba7dbbf616ccb2548be6a4621b3ee707
sutter-dave <sutter@intransix.com>
2021-04-10 17:00:52 -0700

    implemented unregister component view

--- src/componentViewInfo.js
+++ src/componentViewInfo.js
@@ -8,6 +8,12 @@ export function registerComponentView(viewClass) {
     componentViewClassMap[viewClass.componentName] = viewClass;
 }
 
+/** This method is used to unregister a component view. Note that there is no event
+ * fired here. */
+export function unregisterComponentView(viewClass) {
+    delete componentViewClassMap[viewClass.componentName];
+}
+
 /** This method retrieves a component view class using the component unique name. */
 export function getComponentViewClass(componentName) {
     return componentViewClassMap[componentName];
